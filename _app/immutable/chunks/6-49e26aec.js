a2747382af65f9c2a900965ccd0c6e736acfae7d
shlavik <shlavik@users.noreply.github.com>
2023-02-14 02:20:50 +0000

    deploy: e711548be4cb4f5aa9005ed3fe542b3d3ca0aeb0

--- _app/immutable/chunks/6-49e26aec.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/satisfaction-with-life/_page.svelte-b59c3821.js";export{t as component};
--- _app/immutable/chunks/7-3e008039.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/well-being-index/_page.svelte-3dcabfbf.js";export{t as component};
